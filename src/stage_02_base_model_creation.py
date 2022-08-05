b46e10789aa2fd551f3536bbcf56c336d60ccbd3
Rishbah-76 <rbrishabh76@gmail.com>
2022-08-05 23:45:13 +0530

    Added MLFLOW project file with 2 stages

--- src/stage_02_base_model_creation.py
+++ src/stage_02_base_model_creation.py
@@ -3,15 +3,20 @@ import os
 import shutil
 from tqdm import tqdm
 import logging
-from src.utils.common import read_yaml, create_directories
-from src.utils.model_summary import log_model_summary
 import random
 import tensorflow as tf
+
+
+import sys
+sys.path.append('../')
+from src.utils.common import read_yaml, create_directories
+from src.utils.model_summary import log_model_summary
+
 
 STAGE = "Base Model Creation" ## <<< change stage name 
 
 logging.basicConfig(
-    filename=os.path.join("logs", 'running_logs.log'), 
+    filename=os.path.join("../logs", 'running_logs.log'), 
     level=logging.INFO, 
     format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
     filemode="a"
@@ -51,16 +56,16 @@ def main(config_path):
         config['data']['model_dir']
         )
 
-    create_directories([path_to_model])
+    create_directories([path_to_model_dir])
 
-    path_to_model =os.path.join(path_to_model, config['data']['init_model_file'])
+    path_to_model =os.path.join(path_to_model_dir, config['data']['init_model_file'])
     classifier.save(path_to_model)
     logging.info(f"Model is saved to {path_to_model}")
 
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
-    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--config", "-c", default="../configs/config.yaml")
     parsed_args = args.parse_args()
 
     try:
